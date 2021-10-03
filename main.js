d2804c918e231af5b39eca71a87442683b36bdbb
Katherine Langley <promoteglobal@gmail.com>
2021-10-03 16:43:14 +0800

    added comments

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
 
+//return the doubled value of the number, if over 9 after doubling, then subtract 9 and that will be the returned value.
 const getConvertedVal = (arr, i, value, dbldvlue) => {
   dbldvlue = arr[i] * 2;
   value = dbldvlue;
@@ -32,13 +33,13 @@ const getConvertedVal = (arr, i, value, dbldvlue) => {
   return value;
 };
 
-// Add your functions below:
+
 const validateCred = (arr) => {
   let sum = 0;
 
-  //loop through arrays and determine even from odd numbered credit cards
+  //loop through each indivual number on the card and calculate if the whole number is invalid. return true if valid, false if not.
   for(let i = arr.length - 1; i >= 0; i--) {
-    let value = arr[i]; //get values for credit card individual numbers that don't need to be calculated
+    let value = arr[i]; //get values for credit card individual numbers that don't need to be calculated (every other number)
     if(arr.length % 2 === 0) { //get values for credit card individual numbers that need to be calculated for even number placements of credit cards.
       if(i % 2 === 0) {
        value = getConvertedVal(arr, i);  
@@ -50,7 +51,7 @@ const validateCred = (arr) => {
     }
     sum += value;
   }
-    if(sum % 10 === 0) { //return true if valid, false if invalid.
+    if(sum % 10 === 0) { //if sum is divided by 10 === 0  then numbers are valid, if not, the numbers are invalid.
       return true;
     } else {
       return false;
@@ -58,21 +59,21 @@ const validateCred = (arr) => {
 };
 
 
-//gather an array of all invalid credit card numbers
+//put in an array of all invalid and valid credit card numbers.  Send each card number to validateCred and return and array of invalid numbers.
 const findInvalidCards = (arrs) => {
   const invalidNums = arrs.filter(arr => !validateCred(arr));
   return invalidNums;
 };
 
-
+// Put in the everything array of all numbers and return the array of invalid cards.
 const arrOfInvalidNums = findInvalidCards(batch);
 
-//gather an array of credit card company names that gives out invalid credit card numbers.
+//put in array of invalid credit card numbers and return arr of companies that issued the card.
 const idInvalidCardCompanies = (invalidArrs) => {
   let company = "";
   const invalidCompanies = [];
   for(let i = 0; i < invalidArrs.length; i++) {
-    const firstDigit = invalidArrs[i][0];
+    const firstDigit = invalidArrs[i][0]; //Identify company by knowing which number the card starts with.
     switch(firstDigit) {
       case 3:
         company = 'Amex (American Express)';
@@ -92,10 +93,12 @@ const idInvalidCardCompanies = (invalidArrs) => {
     invalidCompanies.push(company);
   }
 
+  //put in array of invalid companies, take out duplicates and return array of invalid companies without duplicates.
   function onlyUnique(value, index, self) { //filter out duplicate companies.
     return self.indexOf(value) === index;
     }
-    const uniqueInvalidCompanies = invalidCompanies.filter(onlyUnique);
+    
+  const uniqueInvalidCompanies = invalidCompanies.filter(onlyUnique);
     return uniqueInvalidCompanies; 
 };
 
